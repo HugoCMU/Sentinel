1de594b9cf25fd636f2f56f8de013cf6f7e0de2e
HugoCMU <hugo.p.cmu@gmail.com>
2016-05-21 17:28:16 -0400

    Finished snapshot function, fixed up calibration function and depth tracking function

--- camera_calibration/OpenCVCalibration/calibrator_tutorial.py
+++ camera_calibration/OpenCVCalibration/calibrator_tutorial.py
@@ -1,60 +1,103 @@
+#!/usr/bin/env python
+# 03/14/2016 HugoCMU
+# Program will get calibration matrices for cameras
+
 import numpy as np
 import cv2
 import glob
+import os
 
+import json
+
+import cPickle
+
+# Define wait time for images
+IMSHOW_WAIT_TIME = 3000 # 3 seconds
+
+# Make sure we use proper directory
+os.chdir(os.path.expanduser("~") + '/Sentinel/camera_calibration/OpenCVCalibration')
+
+# Size of Checkerboard
+CHECK_SIZE = (9, 7) # 6 by 8 board
+CHECK_DIMM = 0.023 #23 mm
+
+# Name variables for files
+CALIB_FILENAME = 'calib/camera_calib_data_'
+CAMERA_NAME = 'L'
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+def display_image(image, frame_name="Image"):
+    '''
+        Displays given image in a named OpenCV window
+    '''
+    cv2.imshow(frame_name, image)
+    cv2.moveWindow(frame_name, 30, 30)
+    cv2.waitKey(IMSHOW_WAIT_TIME)
+    cv2.destroyAllWindows()
+
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((CHECK_SIZE[1]*CHECK_SIZE[0],3), np.float32)
+objp[:,:2] = np.mgrid[0:CHECK_SIZE[0],0:CHECK_SIZE[1]].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('*.jpg')
+# Take images from tmp directory
+images = glob.glob('tmp/img' + CAMERA_NAME + '_*.png')
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+for i, fname in enumerate(images):
+
+    gray = cv2.imread(fname, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    img = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (CHECK_SIZE[0],CHECK_SIZE[1]), None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
 
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+        cv2.cornerSubPix(gray, corners,(11,11), (-1,-1),criteria)
+        imgpoints.append(corners)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+        cv2.drawChessboardCorners(img, (CHECK_SIZE[0],CHECK_SIZE[1]), corners, ret)
+
+        # display_image(img, frame_name="Frame %s" % i)
+
 
 cv2.destroyAllWindows()
 
 # returns the camera matrix, distortion coefficients, rotation and translation vectors
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
 
-# 
-img = cv2.imread('left12.jpg')
+img = cv2.imread('tmp/imgR_2.png')
 h,  w = img.shape[:2]
 newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+
+display_image(img, frame_name="Uncalibrated")
+
+# Write calibration matrices into pickle file
+with open(CALIB_FILENAME + CAMERA_NAME + '.txt', "w") as f:
+    cPickle.dump((mtx, dist, newcameramtx), f)
 
 # undistort
 dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
 
+display_image(dst, frame_name="Calibrated")
+
 # crop the image
 x,y,w,h = roi
 dst = dst[y:y+h, x:x+w]
-cv2.imwrite('calibresult.png',dst)
 
-# Determine effectiveness of calibratio
-nmean_error = 0
+display_image(dst, frame_name="Calibrated and Cropped")
+
+# Determine effectiveness of calibration
+tot_error = 0
+mean_error = 0
 for i in xrange(len(objpoints)):
     imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
     error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
--- camera_calibration/OpenCVCalibration/snapshot.py
+++ camera_calibration/OpenCVCalibration/snapshot.py
@@ -9,40 +9,52 @@ import os
 import time
 
 import sys
+import getopt
 
+import numpy as np
 
 # ----------------- GET PROPER FILENAMES & PATHS
+# Define wait time for images
+IMSHOW_WAIT_TIME = 3000 # 3 seconds
 
-# Make sure we use proper directory
-os.chdir('$HOME/Sentinel/camera_calibration/OpenCVCalibration/')
+# Make sure we use proper directory (tmp folder by default)
+os.chdir(os.path.expanduser("~") + '/Sentinel/camera_calibration/OpenCVCalibration/tmp')
 
-# Define name of camera calibration images: calib_image_right_XXX.png
-filename = 'img'
+# Define name of camera calibration img_R_XXX.png
+FILENAME_PREFIX = 'img'
+
+# folder name for current session
+foldername = time.strftime("%d_%m_%Y__%H_%M")
 
 # ----------------- TAKE USER INPUT
 
 try:
-	pts, args = getopt.getopt(argv,"op")
+	# command line arguments
+	for arg in getopt.getopt(sys.argv,"op")[1]:
+		if arg in ("-o"):
+			print("-o argument used, overwriting images in tmp")
+
+			# Remove current pictures in tmp folder
+			for f in (f for f in os.listdir(".")):
+				os.remove(f)
+
+		elif arg in ("-p"): # TODO: Allow for long option that defines folder name
+			print("-p argument used, creating new folder in hist")
+
+			# Exit the tmp folder
+			os.chdir('../hist')
+
+			# Create/enter date folder if it does not exist
+			if not os.path.exists(foldername):
+				os.makedirs(foldername)
+				os.chdir(foldername)
+
 except getopt.GetoptError:
 	print("\n")
 	print("-o 	overwrite current generated images in tmp")
 	print("-p  push generated images into new date folder")
 	print("\n")
-
 	sys.exit(2)
-
-img_iter = 0
-
-for opt, arg in opts:
-	if opt in ("-o"):
-		pass
-	elif opt in ("-p"): # TODO: Allow for long option that defines folder name
-
-		filename = "hist/" + time.strftime("%d/%m/%Y_%H:%M:%S") + "/" + filename
-
-if not opts:
-	while os.path.exists(filename_right + '%s.png' % img_iter):
-		img_iter += 1
 
 # --------------------------- CAPTURE IMAGES
 
@@ -62,21 +74,50 @@ def rotate_imgs(raw_image_right, raw_image_left):
 
 	return image_right, image_left
 
+def display_image(image, frame_name="Image"):
+    '''
+        Displays given image in a named OpenCV window
+    '''
+    cv2.imshow(frame_name, image)
+    cv2.moveWindow(frame_name, 30, 30)
+    cv2.waitKey(IMSHOW_WAIT_TIME)
+    cv2.destroyAllWindows()
+    
+def display_double_image(image_L, image_R, frame_name="Image"):
+	'''
+		Displays two images (mono) in one OpenCV window
+	'''
+
+	try:
+		new_img = np.hstack((image_L, image_R))
+	except ValueError:
+		print("Cannot diplay: dimmension mismatch in pictures")
+		return
+
+	# Show snapshot
+	display_image(new_img, frame_name)
+
 # Create camera capture objects
-cam_left = cv2.VideoCapture(0)
-cam_right = cv2.VideoCapture(1)
+cam_left = cv2.VideoCapture(1)
+cam_right = cv2.VideoCapture(0)
+
+# Counter for number of snapshots taken
+snapshot_iter = 0
 
 # Keep taking pictures until user exits the program
 while True:
 
-	# Give user time to move board
-	for i in xrange(3):
-		print("%s.....", (3 - i))
-		time.sleep(1)
+	# # Give user time to move board
+	# for i in xrange(3):
+	# 	print("Countdown: %s....." % (3 - i))
+	# 	time.sleep(1)
 
 	# Grab image directly from cv2 video capture object
-	foo, cv_image_left = cam_left.read()
-	foo, cv_image_right = cam_right.read()
+	# TODO: Put camera reading in thread, so latest image is easier to extract from image buffer
+	# http://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+	for _ in range(5): 
+		foo, cv_image_right = cam_right.read()
+		foo, cv_image_left = cam_left.read()
 
 	# Convert color images to gray for calibration
 	image_right_mono = cv2.cvtColor(cv_image_right, cv2.COLOR_BGR2GRAY)
@@ -86,15 +127,14 @@ while True:
 	image_right, image_left = rotate_imgs(image_right_mono, image_left_mono)
 
 	# Save images to file
-	cv2.imwrite(filename + 'R_%s.png' % snapshot_iter, image_right)
-	cv2.imwrite(filename + 'L_%s.png' % snapshot_iter, image_left)
+	cv2.imwrite(FILENAME_PREFIX + 'R_%s.png' % snapshot_iter, image_right)
+	cv2.imwrite(FILENAME_PREFIX + 'L_%s.png' % snapshot_iter, image_left)
 
 	# Increase image counter
 	snapshot_iter += 1
 
 	# Visual confirmation
-	print("Snapshot - %s", snapshot_iter)
+	print("Snapshot #%s Taken!" % snapshot_iter)
 
-
-
-
+	# Display images
+	display_double_image(image_left, image_right)
